e97989cf075a491634c8f355efee36fa8ca071fd
Spencer Au <spencerau96@gmail.com>
2022-11-14 16:29:08 -0800

    Update Faculty.cpp

--- Faculty.cpp
+++ Faculty.cpp
@@ -19,3 +19,18 @@ Faculty::~Faculty() {
 int Faculty::getid() {
     return id;
 }
+
+void Faculty::addStudent(int id) {
+    students->addBack(id);
+}
+
+void Faculty::removeStudent(int id) {
+    for (int i = 0; i < students->getSize(); i++) {
+        if (students->get(i) == id) students->remove(i);
+    }
+}
+
+bool Faculty::hasStudent(int id) {
+    if (students->contains(id)) return true;
+    else return false;
+}
